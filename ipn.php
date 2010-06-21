c3d12d77174ff3639eec7678e8f6eb696445d44c
prasya <volodymyr.prasolov@.edvantis.com>
2010-06-21 12:20:50 +0300

    ipn.php file has been changed

--- ipn.php
+++ ipn.php
@@ -1,22 +1,45 @@
-<?php
 
-mysql_connect("localhost", "user", "password") or die(mysql_error());
-mysql_select_db("DBName") or die(mysql_error());
-
-
+ <?php
 // read the post from PayPal system and add 'cmd'
 $req = 'cmd=_notify-validate';
+
 foreach ($_POST as $key => $value) {
 $value = urlencode(stripslashes($value));
 $req .= "&$key=$value";
 }
+
 // post back to PayPal system to validate
+	
 $header = "POST /cgi-bin/webscr HTTP/1.0\r\n";
+ 
+	// If testing on Sandbox use: 
+	// $header .= "Host: www.sandbox.paypal.com:443\r\n";
+$header .= "Host: www.paypal.com:443\r\n";
 $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
 $header .= "Content-Length: " . strlen($req) . "\r\n\r\n";
 
-$fp = fsockopen ('ssl://www.sandbox.paypal.com', 443, $errno, $errstr, 30);
+	// If testing on Sandbox use:
+	//$fp = fsockopen ('ssl://www.sandbox.paypal.com', 443, $errno, $errstr, 30);
+$fp = fsockopen ('ssl://www.paypal.com', 443, $errno, $errstr, 30);
 
+// assign posted variables to local variables
+$item_name = $_POST['item_name'];
+$item_number = $_POST['item_number'];
+$payment_status = $_POST['payment_status'];
+$payment_amount = $_POST['mc_gross'];
+$payment_currency = $_POST['mc_currency'];
+$txn_id = $_POST['txn_id'];
+$receiver_email = $_POST['receiver_email'];
+$payer_email = $_POST['payer_email'];
+
+//set email variables
+$From_email = "From: user@com.ua";
+$Subject_line = "PayPal";
+
+$email_msg = "Thanks for purchasing my item. Your order will be delivered in 3-4 days. We appreciate your business.";
+$email_msg .= "\n\nThe details of your order are as follows:";
+$email_msg .= "\n\n" . "Transaction ID: " .  $txn_id ;
+$email_msg .= "\n" . "Payment Date: " . $payment_date;
 
 if (!$fp) {
 // HTTP ERROR
@@ -25,56 +48,34 @@ fputs ($fp, $header . $req);
 while (!feof($fp)) {
 $res = fgets ($fp, 1024);
 if (strcmp ($res, "VERIFIED") == 0) {
+// check the payment_status is Completed
+// check that txn_id has not been previously processed
+// check that receiver_email is your Primary PayPal email
+// check that payment_amount/payment_currency are correct
+// process payment
 
-// PAYMENT VALIDATED & VERIFIED!
+$mail_From = $From_email;
+$mail_To = $payer_email;
+$mail_Subject = $Subject_line;
+$mail_Body = $email_msg;
 
-$email = $_POST['payer_email'];
-$password = mt_rand(1000, 9999);
-
-mysql_query("INSERT INTO users (email, password) VALUES('". mysql_escape_string($email) ."', '".md5($password)."' ) ") or die(mysql_error()); 
-
-$to      = $email;
-$subject = 'Download Area | Login credentials';
-$message = '
-
-Thank you for your purchase
-
-Your account information
--------------------------
-Email: '.$email.'
-Password: '.$password.'
--------------------------
-            
-You can now login at http://yourwebsite.com/PayPal/';
-$headers = 'From:noreply@downloadarea.com' . "\r\n";
-
-mail($to, $subject, $message, $headers);
-
+mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
 
 
 }
-
 else if (strcmp ($res, "INVALID") == 0) {
+// log for manual investigation
 
-// PAYMENT INVALID & INVESTIGATE MANUALY!
+$mail_From = $From_email;
+$mail_To = $receiver_email;
+$mail_Subject = "INVALID IPN POST";
+$mail_Body = "INVALID IPN POST. The raw POST string is below.\n\n" . $req;
 
-$to      = 'admin@yourwebsite.com';
-$subject = 'Download Area | Invalid Payment';
-$message = '
-
-Dear Administrator,
-
-A payment has been made but is flagged as INVALID.
-Please verify the payment manualy and contact the buyer.
-
-Buyer Email: '.$email.'
-';
-$headers = 'From:noreply@yourwebsite.com' . "\r\n";
-
-mail($to, $subject, $message, $headers);
+mail($mail_To, $mail_Subject, $mail_Body, $mail_From);
 
 }
 }
 fclose ($fp);
 }
 ?>
+
